4cd77c5834c4fb9d2fb4aebd076a9bcc3a69843f
DilanTank <92825938+DilanTank@users.noreply.github.com>
2023-09-20 10:35:46 -0400

    verify input change

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -9,21 +9,12 @@ class Rectangle():
         return ("A rectangle with width {self.width} and height {self.height}")
 
     def verify_input(self):
-        if self.width == True and self.height == True:
+        if self.width > 0  and self.height > 0:
             return True
         else: 
             return False
 
-
-
-
-    # Create the "verify_input" method
-    #
-    # It returns a boolean
-    #       True if the width and height are positive numbers
-    #       False otherwise
-
-    # YOUR CODE HERE
+    def area (self):
 
 
 
